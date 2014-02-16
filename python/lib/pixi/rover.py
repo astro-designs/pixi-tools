49400561bd5c19b43ec1979e0a7ab08d0e13602f
Simon Cantrill <simonjcantrill@gmail.com>
2014-02-16 16:42:55 +0000

    Add left/right speed mode to pixi-rover

--- python/lib/pixi/rover.py
+++ python/lib/pixi/rover.py
@@ -22,15 +22,31 @@ def pwmSet (pin, value):
 #	print ('pwmWritePin', pin, hex (value))
 	pwmWritePin (spi, pin, value)
 
-motor = [0, 0x8000]
+motorFlags = [0, 0x8000] # forward, reverse
+
 def moveRover (leftSide, rightSide, speed):
 	pwm = (speed * 1023 / 100) & 0x000003ff
 	print ('moveRover', leftSide, rightSide, speed, pwm, hex (pwm))
 	# each side must be synchronised, but each side the motors are opposed:
-	fl = pwm + motor[    leftSide ]
-	bl = pwm + motor[not leftSide ]
-	fr = pwm + motor[    rightSide]
-	br = pwm + motor[not rightSide]
+	fl = pwm + motorFlags[    leftSide ]
+	bl = pwm + motorFlags[not leftSide ]
+	fr = pwm + motorFlags[    rightSide]
+	br = pwm + motorFlags[not rightSide]
+
+	pwmSet (pwmFr, fr)
+	pwmSet (pwmBl, bl)
+	pwmSet (pwmBr, br)
+	pwmSet (pwmFl, fl) # Must write to this pin last!
+
+def moveRoverX (speedL, speedR):
+	pwmL = (abs(speedL) * 1023 / 100) & 0x000003ff
+	pwmR = (abs(speedR) * 1023 / 100) & 0x000003ff
+	print ('moveRoverX', speedL, speedR, hex (pwmL), hex (pwmR))
+	# each side must be synchronised, but each side the motors are opposed:
+	fl = pwmL + motorFlags[speedL < 0]
+	bl = pwmL + motorFlags[speedL > 0]
+	fr = pwmR + motorFlags[speedR < 0]
+	br = pwmR + motorFlags[speedR > 0]
 
 	pwmSet (pwmFr, fr)
 	pwmSet (pwmBl, bl)
