b708089775d5aab745d74364ee4290db53aee17d
Simon Cantrill <simonjcantrill@gmail.com>
2014-08-09 13:51:41 +0100

    libpixi: document flash functions

--- libpixi/pixi/flash.h
+++ libpixi/pixi/flash.h
@@ -30,7 +30,7 @@ LIBPIXI_BEGIN_DECLS
 ///@defgroup PiXiFlash PiXi flash / M25P40 interface
 ///@{
 
-enum
+enum FlashSpec
 {
 	FlashCapacity   =     524288, // 4 megabit
 	FlashPageSize   =        256, // bytes
@@ -40,7 +40,7 @@ enum
 	FlashSectorBaseMask = ~(FlashSectorSize - 1) // bytes
 };
 
-enum StatusBits
+enum FlashStatusBits
 {
 	WriteInProgress            = 1 << 0,
 	WriteEnableLatch           = 1 << 1,
@@ -52,16 +52,55 @@ enum StatusBits
 	StatusRegisterWriteProtect = 1 << 7
 };
 
+///	Open the Pi SPI channel to the pixi flash. When finished,
+///	call pixi_closePixi().
+///	@return 0 on success, or negative error code on error
 int pixi_flashOpen (void);
+
+///	Close the Pi SPI channel to the pixi flash.
+///	@return 0 on success, or negative error code on error
 int pixi_flashClose (void);
+
+///	Execute the 'Release from deep power down and read
+///	electronic signature' instruction.
+///	@return 8 bit unsigned signature value on success, or negative error code on error.
 int pixi_flashRdpReadSig (void);
+
+///	Read the flash device id. Should equal #FlashId.
+///	@return 24 bit unsigned id value on success, or negative error code on error.
 int pixi_flashReadId (void);
+
+///	Read the flash status register.
+///	@see StatusBits
+///	@return 8 bit unsigned bitmap, or negative error code on error.
 int pixi_flashReadStatus (void);
+
+///	Read from the flash memory.
+///	@param	address	flash memory address offset
+///	@param	buffer	destination buffer
+///	@param	length	number of bytes to read
+///	@return number of bytes read, or negative error code on error.
 int pixi_flashReadMemory (uint address, void* buffer, uint length);
-///	Erase the sectors corresponding to the region defined by @a address, @a length.
+
+///	Erase the sectors corresponding to a memory region.
+///	Note that this erases all the sectors (#FlashSectorSize)
+///	that the memory region overlaps.
+///	@param	address	flash memory address offset
+///	@param	length	number of bytes to read
+///	@return number of bytes erased, or negative error code on error.
 int pixi_flashEraseSectors (uint address, uint length);
+
+///	Write to flash memory. Note that the write process only changes
+///	0 value bits to 1, so you may need to erase the memory first,
+///	using @c pixi_flashEraseSectors (@a address, @a length);
+///	@param	address	flash memory address offset
+///	@param	buffer	source buffer
+///	@param	length	number of bytes to write
+///	@return number of bytes written, or negative error code on error.
 int pixi_flashWriteMemory (uint address, const void* buffer, uint length);
 
+///	Erase the entire flash memory.
+///	@return 0 on success, or negative error code on error.
 int pixi_flashBulkErase (void);
 
 ///@} defgroup
